d03c82447b2e7fa5cd4f8b9ada7bb42b2cda6ecf
mdselimme <mdselimdev@gmail.com>
2024-10-26 01:28:10 +0600

    add_join_our_forum_section

--- javascript/add_reading.js
+++ javascript/add_reading.js
@@ -21,16 +21,20 @@ const displayReadPost = (arr) =>{
     const readPostBox = document.getElementById("readpost");
     document.getElementById("read_count").innerText = arr.length;
     readPostBox.textContent = "";
-    arr.forEach((element)=>{
+    if(arr.length===0){
+        readPostBox.innerHTML = `<h1 class="text-center">No post Read</h1>`
+    }else{
+        arr.forEach((element)=>{
        
-        const div = document.createElement("div");
-        div.classList.add("flex", "justify-between", "items-center", "bg-white", "p-3", "rounded-2xl", "mb-7") ;
-        div.innerHTML = `
-             <p class="text-[#12132D] text-base font-semibold">${element.title}</p>
-                            <div>
-                                <span class="mr-5 text-base font-normal text-[#12132D99] flex items-center"><i class="fa-solid fa-eye mr-2"></i> ${element.view_count}</span>
-                            </div>
-        `;
-        readPostBox.appendChild(div);
-    });
+            const div = document.createElement("div");
+            div.classList.add("flex", "justify-between", "items-center", "bg-white", "p-3", "rounded-2xl", "mb-7") ;
+            div.innerHTML = `
+                 <p class="text-[#12132D] text-base font-semibold">${element.title}</p>
+                                <div>
+                                    <span class="mr-5 text-base font-normal text-[#12132D99] flex items-center"><i class="fa-solid fa-eye mr-2"></i> ${element.view_count}</span>
+                                </div>
+            `;
+            readPostBox.appendChild(div);
+        });
+    }
 };
